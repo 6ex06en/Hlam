601f7f4f2e8e7152c7664e0032581b0b0720c949
Kerz <kerzo@mail.ru>
2014-08-14 00:37:32 +0400

    After title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Second App')
     end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Second App | Home")
+    end
   end
 
 
@@ -17,12 +21,20 @@ describe "Help page" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Second App | Help")
+    end
   end
 describe "About page" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Second App | About Us")
     end
   end
 end
